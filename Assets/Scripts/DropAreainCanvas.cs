1e320ec8cac4f36c23ece13406a952329a53814c
FatihAslan388 <69825782+FatihAslan388@users.noreply.github.com>
2022-10-22 02:02:24 +0300

    123

---
+++ Assets/Scripts/DropAreainCanvas.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DropAreainCanvas : MonoBehaviour
+{
+    [SerializeField]
+    Text _countText;
+    [SerializeField]
+    DropArea _dropArea;
+
+    Vector3 _startScale;
+    void Start()
+    {
+        _startScale= GetComponentInChildren<Canvas>().transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _countText.text = (_dropArea.droppedObjects.Count + "/" + _dropArea.stackLimit);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>()!=null)
+        {
+            GetComponentInChildren<Canvas>().enabled = true;
+            DOTween.To(() => new Vector3(0, 0, 0), x => GetComponentInChildren<Canvas>().transform.localScale = x, _startScale, 0.3f).SetEase(Ease.Linear);
+        }
+        
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+        {
+            GetComponentInChildren<Canvas>().enabled = false;
+            DOTween.To(() => _startScale, x => GetComponentInChildren<Canvas>().transform.localScale = x, new Vector3(0, 0, 0), 0.3f).SetEase(Ease.Linear);
+        }
+        
+    }
+}

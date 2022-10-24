8203bd5c90b41b8785d7fe18fc8d2b3e7fc411e7
FatihAslan388 <69825782+FatihAslan388@users.noreply.github.com>
2022-10-24 03:50:28 +0300

    123

--- Assets/Scripts/PressMachine.cs
+++ Assets/Scripts/PressMachine.cs
@@ -8,28 +8,38 @@ public class PressMachine : MonoBehaviour
     [SerializeField]
     public CollectArea _collectArea;
 
-
+    [SerializeField]
+    GameObject _pres1, _pres2;
 
     [SerializeField]
     Material[] _materials;
 
-    Vector3 _startPosition;
+    Vector3 _startPosition1, _startPosition2;
 
     [SerializeField]
-    Transform _endPosition;
+    Transform _endPosition1,_endPosition2;
 
     private void Start()
     {
-        _startPosition = transform.position;
+        _startPosition1 = _pres1.transform.position;
+        _startPosition2 = _pres2.transform.position;
+
     }
     public void Press(float time,GameObject _iron,Transform _machineDropPoint) 
     {
-        transform.DOMove(_endPosition.position,time).OnComplete(()=> 
+        _pres1.transform.DOMove(_endPosition1.position,time).OnComplete(()=> 
         {
-            transform.DOMove(_startPosition, time);
-            _iron.GetComponent<MeshRenderer>().materials = _materials;
-            _iron.transform.DOMove(_machineDropPoint.position, time);
-            _collectArea.AddObjectstoList(_iron);
+            _pres2.transform.DOMove(_endPosition2.position, time).OnComplete(() => 
+            {
+
+                _pres1.transform.DOMove(_startPosition1, time);
+                _pres2.transform.DOMove(_startPosition2, time);
+                _iron.GetComponent<MeshRenderer>().materials = _materials;
+                _iron.transform.DOMove(_machineDropPoint.position, time);
+                _collectArea.AddObjectstoList(_iron);
+
+            });
+
             
         });
     }
